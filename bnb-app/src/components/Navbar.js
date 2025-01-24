e681580bbbef4ae99796b972955b1d8121919c4f
Icexres <kipuaayush@gmail.com>
2025-01-24 07:12:55 +0545

    changed to tailwind defined size for the searchbox bar and kept padding on top div

--- bnb-app/src/components/Navbar.js
+++ bnb-app/src/components/Navbar.js
@@ -4,11 +4,11 @@ import logo from '../assets/images/airbnb.svg';
 const Navbar = () => {
     return ( 
         <>
-        <div className="bg-slate-100 h-24 w-auto flex justify-between ">
-            <div className="logo pt-7 pl-7 pr-20">
+        <div className="bg-slate-100 h-20 w-full flex justify-between px-8">
+            <div className="logo self-center mr-28">
                 <img src={logo} alt='logobnb' className='h-9'></img>
             </div>
-            <div className="middle flex space-x-8 ml-10">
+            <div className="middle flex space-x-8 ">
                 <button><span className='font-bold text-xl'>Stays</span></button>
                 <button><span className='font-semibold text-lg text-gray-500'>Experiences</span></button>
             </div>
@@ -20,8 +20,8 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
-        <div className="bg-slate-50 h-20 w-auto flex justify-center pb-8 border-b-2 border-gray-200">
-            <div className="search-bar-wrapper bg-gray-200 self-center justify-around border-2 border-black flex w-[850px] h-[60px] rounded-full">
+        <div className="bg-slate-50 h-20 w-auto flex justify-center pb-4 border-b-2 border-gray-200">
+            <div className="search-bar-wrapper bg-white self-center justify-around border-2 border-black flex w-screen mx-60 h-14 rounded-full">
                 <div className='text-sm self-center pl-5 pr-28'>where<br></br>search destinations</div>
                 <div className='text-sm self-center'>check in <br></br>Add dates</div>
                 <div className='text-sm self-center'>check out <br></br>Add dates</div>
