3298b19b0b06d62dc7ed5793f2b2806c478a48ec
Icexres <kipuaayush@gmail.com>
2025-01-25 16:17:22 +0545

    fixed typography and css for the searchbox by changing text size to xs and increasing height also created a db folder called options.json to set up dynamic icons through mapping(left to make)

--- bnb-app/src/components/Navbar.js
+++ bnb-app/src/components/Navbar.js
@@ -8,7 +8,7 @@ import { IoSearchCircleSharp } from "react-icons/io5";
 const Navbar = () => {
     return ( 
         <>
-        <div className="bg-slate-100 h-20 w-full flex justify-between px-8">
+        <div className="bg-slate-50 h-20 w-full flex justify-between px-8">
             <div className="logo self-center mr-28">
                 <img src={logo} alt='logobnb' className='h-9'></img>
             </div>
@@ -24,12 +24,12 @@ const Navbar = () => {
                 </div>
             </div>
         </div>
-        <div className="bg-slate-50 h-20 w-auto flex justify-center pb-4 border-b-2 border-gray-200">
-            <div className="search-bar-wrapper bg-white self-center justify-around border-black flex w-screen mx-60 h-14 rounded-full shadow-lg mt-4 divide-x-2">
-                <div className='text-sm self-center pl-5 pr-28 '><span className='font-Cereal'>where<br></br>search destinations</span></div>
-                <div className='text-sm self-center pl-2'><span className='font-Cereal'>Check in <br></br>Add dates</span></div>
-                <div className='text-sm self-center pl-2'><span className='font-Cereal'>Check out <br></br>Add dates</span></div>
-                <div className='text-sm self-center flex pl-2 h-10'>
+        <div className="bg-slate-50 h-24 w-auto flex justify-center pb-4 border-b-2 border-gray-200">
+            <div className="search-bar-wrapper bg-white self-center justify-around border-black flex w-screen mx-60 h-16 rounded-full shadow-md mt-4 divide-x-2">
+                <div className='text-xs self-center pl-5 pr-28 '><span className='font-Cereal'>where<br></br>search destinations</span></div>
+                <div className='text-xs self-center pl-2'><span className='font-Cereal'>Check in <br></br>Add dates</span></div>
+                <div className='text-xs self-center pl-2'><span className='font-Cereal'>Check out <br></br>Add dates</span></div>
+                <div className='text-xs self-center flex pl-2 h-10'>
                     <div className='self-center mr-28'><span className='font-Cereal'>Who<br></br>Add Guests</span></div>
                     <div className="self-center"><IoSearchCircleSharp style={{color:"#ff3859"}} className='h-14 w-14 '/></div>
                 </div>
